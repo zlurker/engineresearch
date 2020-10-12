4f9f7d6ac54691e3d4f3d0b19c086687b364cb3b
Gabriel CHIA <gabriel_chia@NECSAPSIN.COM>
2020-10-12 17:08:57 +0800

    Cleaned up and refined code to a more base level for easier testing. The problem might be registering a wrong window... More test will need to be run

--- GameEngine/GameEngine/GameEngine.cpp
+++ GameEngine/GameEngine/GameEngine.cpp
@@ -37,7 +37,14 @@ void initLights();
 void setViewport(int w, int h);
 void setCamera(float posX, float posY, float posZ, float targetX, float targetY, float targetZ);
 void render();
+GLint MySetPixelFormat();
+bool InitGL(GLvoid);
+GLvoid ReSizeGLScene(GLsizei width, GLsizei height);
 int DrawGLScene(GLvoid);
+
+GLfloat	rtri;				// Angle for the triangle
+GLfloat	rquad;				// Angle for the quad
+const std::wstring wnd_class(L"my_wnd_class");
 
 
 // Forward declarations of functions included in this code module:
@@ -48,7 +55,7 @@ INT_PTR CALLBACK    About(HWND, UINT, WPARAM, LPARAM);
 
 void swapBuffers()
 {
-	::SwapBuffers(hdc);
+
 }
 
 int DrawGLScene(GLvoid)                                 // Here's Where We Do All The Drawing
@@ -60,37 +67,78 @@ int DrawGLScene(GLvoid)                                 // Here's Where We Do Al
 
 void render() {
 	while (true) {
-		std::this_thread::sleep_for(std::chrono::milliseconds(1));
-		glClearColor(0.5f, 0.3f, 0.4f, 1);                       // background color
-		glClear(GL_COLOR_BUFFER_BIT);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);	// Clear screen and depth buffer
+		glLoadIdentity();									// Reset the current modelview matrix
+		glTranslatef(-1.5f, 0.0f, -6.0f);						// Move left 1.5 units and into the screen 6.0
+		glRotatef(rtri, 0.0f, 1.0f, 0.0f);						// Rotate the triangle on the y axis 
+		glBegin(GL_TRIANGLES);								// Start drawing a triangle
+		glColor3f(1.0f, 0.0f, 0.0f);						// Red
+		glVertex3f(0.0f, 1.0f, 0.0f);					// Top Of triangle (front)
+		glColor3f(0.0f, 1.0f, 0.0f);						// Green
+		glVertex3f(-1.0f, -1.0f, 1.0f);					// Left of triangle (front)
+		glColor3f(0.0f, 0.0f, 1.0f);						// Blue
+		glVertex3f(1.0f, -1.0f, 1.0f);					// Right of triangle (front)
+		glColor3f(1.0f, 0.0f, 0.0f);						// Red
+		glVertex3f(0.0f, 1.0f, 0.0f);					// Top Of triangle (right)
+		glColor3f(0.0f, 0.0f, 1.0f);						// Blue
+		glVertex3f(1.0f, -1.0f, 1.0f);					// Left of triangle (right)
+		glColor3f(0.0f, 1.0f, 0.0f);						// Green
+		glVertex3f(1.0f, -1.0f, -1.0f);					// Right of triangle (right)
+		glColor3f(1.0f, 0.0f, 0.0f);						// Red
+		glVertex3f(0.0f, 1.0f, 0.0f);					// Top Of triangle (back)
+		glColor3f(0.0f, 1.0f, 0.0f);						// Green
+		glVertex3f(1.0f, -1.0f, -1.0f);					// Left of triangle (back)
+		glColor3f(0.0f, 0.0f, 1.0f);						// Blue
+		glVertex3f(-1.0f, -1.0f, -1.0f);					// Right of triangle (back)
+		glColor3f(1.0f, 0.0f, 0.0f);						// Red
+		glVertex3f(0.0f, 1.0f, 0.0f);					// Top Of triangle (left)
+		glColor3f(0.0f, 0.0f, 1.0f);						// Blue
+		glVertex3f(-1.0f, -1.0f, -1.0f);					// Left of triangle (left)
+		glColor3f(0.0f, 1.0f, 0.0f);						// Green
+		glVertex3f(-1.0f, -1.0f, 1.0f);					// Right of triangle (left)
+		glEnd();											// Done drawing the pyramid
 
-		setViewport(800, 400);
-
-		//glClear(GL_COLOR_BUFFER_BIT);
-
-		// save the initial ModelView matrix before modifying ModelView matrix
-		glPushMatrix();
-
-		// tramsform camera
-		glTranslatef(0, 0, 20);
-		glRotatef(30, 1, 0, 0);   // pitch
-		glRotatef(40, 0, 1, 0);   // heading
-
-		// draw a triangle
-		glBegin(GL_TRIANGLES);
-		glNormal3f(0, 0, 1);
-		glColor3f(1, 0, 0);
-		glVertex3f(3, -2, 0);
-		glColor3f(0, 1, 0);
-		glVertex3f(0, 2, 0);
-		glColor3f(0, 0, 1);
-		glVertex3f(-3, -2, 0);
+		glLoadIdentity();									// Reset the current modelview matrix
+		glTranslatef(1.5f, 0.0f, -7.0f);						// Move right 1.5 units and into the screen 7.0
+		glRotatef(rquad, 1.0f, 1.0f, 1.0f);					// Rotate the quad on the x axis 
+		glBegin(GL_QUADS);									// Draw a quad
+		glColor3f(0.0f, 1.0f, 0.0f);						// Set The color to green
+		glVertex3f(1.0f, 1.0f, -1.0f);					// Top Right of the quad (top)
+		glVertex3f(-1.0f, 1.0f, -1.0f);					// Top Left of the quad (top)
+		glVertex3f(-1.0f, 1.0f, 1.0f);					// Bottom left of the quad (top)
+		glVertex3f(1.0f, 1.0f, 1.0f);					// Bottom right of the quad (top)
+		glColor3f(1.0f, 0.5f, 0.0f);						// Set The color to orange
+		glVertex3f(1.0f, -1.0f, 1.0f);					// Top Right of the quad (bottom)
+		glVertex3f(-1.0f, -1.0f, 1.0f);					// Top Left of the quad (bottom)
+		glVertex3f(-1.0f, -1.0f, -1.0f);					// Bottom left of the quad (bottom)
+		glVertex3f(1.0f, -1.0f, -1.0f);					// Bottom right of the quad (bottom)
+		glColor3f(1.0f, 0.0f, 0.0f);						// Set The color to red
+		glVertex3f(1.0f, 1.0f, 1.0f);					// Top Right of the quad (front)
+		glVertex3f(-1.0f, 1.0f, 1.0f);					// Top Left of the quad (front)
+		glVertex3f(-1.0f, -1.0f, 1.0f);					// Bottom left of the quad (front)
+		glVertex3f(1.0f, -1.0f, 1.0f);					// Bottom right of the quad (front)
+		glColor3f(1.0f, 1.0f, 0.0f);						// Set The color to yellow
+		glVertex3f(1.0f, -1.0f, -1.0f);					// Top Right of the quad (back)
+		glVertex3f(-1.0f, -1.0f, -1.0f);					// Top Left of the quad (back)
+		glVertex3f(-1.0f, 1.0f, -1.0f);					// Bottom left of the quad (back)
+		glVertex3f(1.0f, 1.0f, -1.0f);					// Bottom right of the quad (back)
+		glColor3f(0.0f, 0.0f, 1.0f);						// Set The color to blue
+		glVertex3f(-1.0f, 1.0f, 1.0f);					// Top Right of the quad (left)
+		glVertex3f(-1.0f, 1.0f, -1.0f);					// Top Left of the quad (left)
+		glVertex3f(-1.0f, -1.0f, -1.0f);					// Bottom left of the quad (left)
+		glVertex3f(-1.0f, -1.0f, 1.0f);					// Bottom right of the quad (left)
+		glColor3f(1.0f, 0.0f, 1.0f);						// Set The color to violet
+		glVertex3f(1.0f, 1.0f, -1.0f);					// Top Right of the quad (right)
+		glVertex3f(1.0f, 1.0f, 1.0f);					// Top Left of the quad (right)
+		glVertex3f(1.0f, -1.0f, 1.0f);					// Bottom left of the quad (right)
+		glVertex3f(1.0f, -1.0f, -1.0f);					// Bottom right of the quad (right)
 		glEnd();
 
-		glPopMatrix();
+		rtri += 0.2f;											// Increase the rotation variable for the triangle
+		rquad -= 0.15f;
 		//if (DrawGLScene() == 0)
 			//::MessageBox(0, L"Error with draw", L"Error", MB_ICONEXCLAMATION | MB_OK);
-		swapBuffers();
+		::SwapBuffers(hdc);
 	}
 }
 
@@ -115,12 +163,11 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
 		return FALSE;
 	}
 
+	std::thread thread(render);
 
 	HACCEL hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_GAMEENGINE));
 
 	MSG msg;
-
-	std::thread thread(render);
 
 	// Main message loop:
 	while (GetMessage(&msg, nullptr, 0, 0))
@@ -145,24 +192,20 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
 //
 ATOM MyRegisterClass(HINSTANCE hInstance)
 {
-	WNDCLASSEXW wcex;
+	WNDCLASS wcex;
 
-	wcex.cbSize = sizeof(WNDCLASSEX);
-
-	wcex.style = CS_HREDRAW | CS_VREDRAW;
+	wcex.style = CS_HREDRAW | CS_VREDRAW | CS_OWNDC;
 	wcex.lpfnWndProc = WndProc;
 	wcex.cbClsExtra = 0;
 	wcex.cbWndExtra = 0;
 	wcex.hInstance = hInstance;
 	wcex.hIcon = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_GAMEENGINE));
 	wcex.hCursor = LoadCursor(nullptr, IDC_ARROW);
-	wcex.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
-	wcex.lpszMenuName = MAKEINTRESOURCEW(IDC_GAMEENGINE);
-	wcex.lpszClassName = szWindowClass;
-	wcex.hIconSm = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
-	wcex.style = CS_OWNDC;
+	wcex.hbrBackground = NULL;
+	wcex.lpszMenuName = NULL;
+	wcex.lpszClassName = L"Test";
 
-	return RegisterClassExW(&wcex);
+	return RegisterClass(&wcex);
 }
 
 //
@@ -184,12 +227,9 @@ BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
 	style = WS_OVERLAPPEDWINDOW | WS_VISIBLE | WS_CLIPSIBLINGS | WS_CLIPCHILDREN;
 	HWND hWnd = CreateWindowEx(WS_EX_CLIENTEDGE, szWindowClass, szTitle, style, CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, nullptr, nullptr, hInstance, nullptr);
 
-	//CreateWindowW(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
-	//CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, nullptr, nullptr, hInstance, nullptr);
-
-
 	if (!hWnd)
 	{
+		::MessageBox(0, L"Failed", L"Error", MB_ICONEXCLAMATION | MB_OK);
 		return FALSE;
 	}
 
@@ -197,37 +237,19 @@ BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
 	UpdateWindow(hWnd);
 
 	hdc = ::GetDC(hWnd);
-	if (!setPixelFormat(hdc, 32, 24, 8))
-	{
-		::MessageBox(0, L"Cannot set a suitable pixel format.", L"Error", MB_ICONEXCLAMATION | MB_OK);
-		::ReleaseDC(hWnd, hdc);                     // remove device context
-		return false;
-	}
+	MySetPixelFormat();
 
-	if (!(hglrc = ::wglCreateContext(hdc))) {
-		::MessageBox(0, L"hglrc not set up.", L"Error", MB_ICONEXCLAMATION | MB_OK);
-	}
-	//::ReleaseDC(hWnd, hdc);
+	if (!InitGL())
+		::MessageBox(0, L"Failed", L"Error", MB_ICONEXCLAMATION | MB_OK);
 
-	if (!::wglMakeCurrent(hdc, hglrc)) {
-		::MessageBox(0, L"make current not setup.", L"Error", MB_ICONEXCLAMATION | MB_OK);
-	}
+	ReSizeGLScene(640, 480);
 
-	iniGL();
-	RECT rect;
-
-	::GetClientRect(hWnd, &rect);
-	//::MessageBox(0, L"Set Rect.", L"Error", MB_ICONEXCLAMATION | MB_OK);
-
-	//::MessageBox(0, L"Set Viewport.", L"Error", MB_ICONEXCLAMATION | MB_OK);
-
-
-	//while (true);
-
+	rtri = 0.0f;
+	rquad = 0.0f;
 	return TRUE;
 }
 
-void setViewport(int w, int h) {// set viewport to be the entire window
+/*void setViewport(int w, int h) {// set viewport to be the entire window
 	glViewport(0, 0, (GLsizei)w, (GLsizei)h);
 
 	// set perspective viewing frustum
@@ -263,7 +285,7 @@ void iniGL() {
 	glClearStencil(0);                              // clear stencil buffer
 	glClearDepth(1.0f);
 
-	
+
 	glDepthFunc(GL_LEQUAL);
 
 	initLights();
@@ -295,9 +317,95 @@ void setCamera(float posX, float posY, float posZ, float targetX, float targetY,
 	glMatrixMode(GL_MODELVIEW);
 	glLoadIdentity();
 	gluLookAt(posX, posY, posZ, targetX, targetY, targetZ, 0, 1, 0); // eye(x,y,z), focal(x,y,z), up(x,y,z)
+}*/
+
+GLint MySetPixelFormat()
+{
+	PIXELFORMATDESCRIPTOR pfd = {
+		sizeof(PIXELFORMATDESCRIPTOR),    // size of this pfd 
+		1,                                // version number 
+		PFD_DRAW_TO_WINDOW |              // support window 
+		PFD_SUPPORT_OPENGL |              // support OpenGL 
+		PFD_DOUBLEBUFFER,                 // double buffered 
+		PFD_TYPE_RGBA,                    // RGBA type 
+		24,                               // 24-bit color depth 
+		0, 0, 0, 0, 0, 0,                 // color bits ignored 
+		0,                                // no alpha buffer 
+		0,                                // shift bit ignored 
+		0,                                // no accumulation buffer 
+		0, 0, 0, 0,                       // accum bits ignored 
+		32,                               // 32-bit z-buffer     
+		0,                                // no stencil buffer 
+		0,                                // no auxiliary buffer 
+		PFD_MAIN_PLANE,                   // main layer 
+		0,                                // reserved 
+		0, 0, 0                           // layer masks ignored 
+	};
+
+	GLint  iPixelFormat;
+
+	// get the device context's best, available pixel format match 
+	if ((iPixelFormat = ChoosePixelFormat(hdc, &pfd)) == 0)
+	{
+		::MessageBox(0, L"Set Viewport.", L"Error", MB_ICONEXCLAMATION | MB_OK);
+		return 0;
+	}
+
+	// make that match the device context's current pixel format 
+	if (SetPixelFormat(hdc, iPixelFormat, &pfd) == FALSE)
+	{
+		::MessageBox(0, L"Set Viewport.", L"Error", MB_ICONEXCLAMATION | MB_OK);
+		return 0;
+	}
+
+	if ((hglrc = wglCreateContext(hdc)) == NULL)
+	{
+		::MessageBox(0, L"Set Viewport.", L"Error", MB_ICONEXCLAMATION | MB_OK);
+		return 0;
+	}
+
+	if ((wglMakeCurrent(hdc, hglrc)) == NULL)
+	{
+		::MessageBox(0, L"Set Viewport.", L"Error", MB_ICONEXCLAMATION | MB_OK);
+		return 0;
+	}
+
+	::MessageBox(0, L"Sucess.", L"Error", MB_ICONEXCLAMATION | MB_OK);
+	return 1;
 }
 
-bool setPixelFormat(HDC hdc, int colorBits, int depthBits, int stencilBits)
+bool InitGL(GLvoid)										// All setup for opengl goes here
+{
+	glShadeModel(GL_SMOOTH);							// Enable smooth shading
+	glClearColor(0.0f, 0.0f, 0.0f, 0.5f);				// Black background
+	glClearDepth(1.0f);									// Depth buffer setup
+	glEnable(GL_DEPTH_TEST);							// Enables depth testing
+	glDepthFunc(GL_LEQUAL);								// The type of depth testing to do
+	glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST);	// Really nice perspective calculations
+	return TRUE;										// Initialisation went ok
+}
+
+GLvoid ReSizeGLScene(GLsizei width, GLsizei height)		// Resize and initialise the gl window
+{
+	if (height == 0)										// Prevent A Divide By Zero By
+	{
+		height = 1;										// Making Height Equal One
+	}
+
+	//glViewport(0, 0, width, height);						// Reset The Current Viewport
+
+	glMatrixMode(GL_PROJECTION);						// Select The Projection Matrix
+	glLoadIdentity();									// Reset The Projection Matrix
+
+	// Calculate The Aspect Ratio Of The Window
+	gluPerspective(45.0f, (GLfloat)width / (GLfloat)height, 0.1f, 100.0f);
+
+	glMatrixMode(GL_MODELVIEW);							// Select The Modelview Matrix
+	glLoadIdentity();									// Reset The Modelview Matrix
+}
+
+
+/*bool setPixelFormat(HDC hdc, int colorBits, int depthBits, int stencilBits)
 {
 	PIXELFORMATDESCRIPTOR pfd;
 
@@ -314,7 +422,7 @@ bool setPixelFormat(HDC hdc, int colorBits, int depthBits, int stencilBits)
 		return false;
 
 	return true;
-}
+}*/
 
 ///////////////////////////////////////////////////////////////////////////////
 // find the best pixel format
